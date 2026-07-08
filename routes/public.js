45af7df0fc98092329711ce3a3ef6d6c740eeead
Dimitri-Stocker <dimistocker@gmail.com>
2026-07-08 19:21:43 -0300

    Implementação de banco de dados e ORM

--- routes/public.js
+++ routes/public.js
@@ -1,17 +1,24 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
 
-let users = [];
+const prisma = new PrismaClient();
 
-router.get('/users', (req, res) => {
-    res.json(users);
+router.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
 })
 
-router.post('/users', (req, res) => {
-    const user = req.body;
-    users.push(user);
-    res.status(201).json(user);
+router.post('/users', async (req, res) => {
+
+    const {name, email, age} = req.body
+
+    await prisma.user.create({
+        data: {name, email, age}
+    });
+
+    res.status(201).json({ message: `Usuário ${name} criado com sucesso.`});
 })
 
 export default router
