19b2102902d321ad09a78d60f28a27bf71e676b2
Dimitri-Stocker <dimistocker@gmail.com>
2026-07-08 20:54:14 -0300

    Desenvolvimento de rotas

--- routes/public.js
+++ routes/public.js
@@ -8,7 +8,21 @@ const prisma = new PrismaClient();
 router.get('/users', async (req, res) => {
     const users = await prisma.user.findMany();
     res.status(200).json(users);
-})
+});
+
+router.get('/users/:id', async (req, res) => {
+    const {id} = req.params;
+
+    const user = await prisma.user.findUnique({
+        where: {id}
+    });
+
+    if (!user){
+        return res.status(404).json({message: `Usuário não encontrado.`})
+    };
+
+    res.status(200).json(user);
+});
 
 router.post('/users', async (req, res) => {
 
@@ -19,6 +33,27 @@ router.post('/users', async (req, res) => {
     });
 
     res.status(201).json({ message: `Usuário ${name} criado com sucesso.`});
-})
+});
+
+router.put('/users/:id', async (req, res) => {
+    const {name, email, age} = req.body
+    const {id} = req.params
+    
+    await prisma.user.update({
+        where: {id},
+        data: {name, email, age}
+    });
+
+    res.status(200).json({message: `Usuário ${name} atualizado com sucesso.`});
+});
+
+router.delete('/users/:id', async (req, res) => {
+    const {id} = req.params
+
+    await prisma.user.delete({
+        where: {id}
+    });
+    res.status(200).json({message: `Usuário deletado com sucesso.`});
+});
 
 export default router
