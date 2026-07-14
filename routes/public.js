b2877371898549085367c75e3f9e5910a815ebc5
Dimitri-Stocker <dimistocker@gmail.com>
2026-07-13 22:44:43 -0300

    Aprimoramento do código das rotas

--- routes/public.js
+++ routes/public.js
@@ -6,54 +6,113 @@ const router = Router();
 const prisma = new PrismaClient();
 
 router.get('/users', async (req, res) => {
-    const users = await prisma.user.findMany();
-    res.status(200).json(users);
+    try {
+        const users = await prisma.user.findMany();
+
+        res.status(200).json(users);
+    } catch (error) {
+        console.error(error)
+
+        res.status(500).json({ message: `Erro ao buscar usuários.` });
+    }
 });
 
 router.get('/users/:id', async (req, res) => {
-    const {id} = req.params;
+    try {
+        const { id } = req.params;
 
-    const user = await prisma.user.findUnique({
-        where: {id}
-    });
+        const user = await prisma.user.findUnique({
+            where: { id }
+        });
 
-    if (!user){
-        return res.status(404).json({message: `Usuário não encontrado.`})
-    };
+        if (!user) {
+            return res.status(404).json({ message: `Usuário não encontrado.` })
+        };
 
-    res.status(200).json(user);
+        res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({ message: `Erro ao buscar usuário.` });
+    }
+
 });
 
 router.post('/users', async (req, res) => {
+    try {
+        const { name, email, age } = req.body
 
-    const {name, email, age} = req.body
+        if (!name || !email || !age) {
+            return res.status(400).json({ message: `Nome, email e idade são obrigatórios.` })
+        }
 
-    await prisma.user.create({
-        data: {name, email, age}
-    });
+        const userExists = await prisma.user.findUnique({
+            where: { email }
+        })
 
-    res.status(201).json({ message: `Usuário ${name} criado com sucesso.`});
+        if (userExists) {
+            return res.status(409).json({ message: `Email já cadastrado.` });
+        }
+
+        await prisma.user.create({
+            data: { name, email, age }
+        });
+
+        res.status(201).json({ message: `Usuário ${name} criado com sucesso.` });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({ message: `Erro interno do servidor.` });
+    }
 });
 
 router.put('/users/:id', async (req, res) => {
-    const {name, email, age} = req.body
-    const {id} = req.params
-    
-    await prisma.user.update({
-        where: {id},
-        data: {name, email, age}
-    });
+    try {
+        const { name, email, age } = req.body
+        const { id } = req.params
 
-    res.status(200).json({message: `Usuário ${name} atualizado com sucesso.`});
+        const userExists = await prisma.user.findUnique({
+            where: { id }
+        })
+
+        if (!userExists) {
+            return res.status(404).json({ message: `Usuário não encontrado.` });
+        }
+
+        await prisma.user.update({
+            where: { id },
+            data: { name, email, age }
+        });
+
+        res.status(200).json({ message: `Usuário ${name} atualizado com sucesso.` });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: `Erro interno do servidor.` });
+    }
+
 });
 
 router.delete('/users/:id', async (req, res) => {
-    const {id} = req.params
+    try {
+        const { id } = req.params
 
-    await prisma.user.delete({
-        where: {id}
-    });
-    res.status(200).json({message: `Usuário deletado com sucesso.`});
+        const userExists = await prisma.user.findUnique({
+            where: {id}
+        })
+
+        if (!userExists){
+            return res.status(409).json({message:`Usuário não encontrado.`})
+        }
+
+        await prisma.user.delete({
+            where: { id }
+        });
+        res.status(200).json({ message: `Usuário deletado com sucesso.` });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message:`Erro interno do servidor.`})
+    }
+
 });
 
 export default router
